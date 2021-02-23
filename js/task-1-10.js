dab765012872176fff57890ca1227b89970d5630
Max M <maxminullin@gmail.com>
2021-02-23 23:30:15 +0200

    add

---
+++ js/task-1-10.js
@@ -0,0 +1,57 @@
+import users from './users.js';
+
+//TASK 1
+const getUserNames = users => users.map(user => user.name);
+
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony'  
+
+
+
+//TASK 2
+const getUsersWithEyeColor = (users, color) => users.filter(user => user.eyeColor === color);
+
+console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+
+
+
+//TASK 3
+const getUsersWithGender = (users, gender) => users.filter(user => user.gender === gender);
+  
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+
+
+
+//TASK 4
+const getInactiveUsers = users => users.filter(user => !user.isActive);
+
+console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+
+
+
+//TASK 5
+const getUserWithEmail = (users, email) => users.find(user => user.email === email);
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+
+
+//TASK 6
+const getUsersWithAge = (users, min, max) => users.filter(user => {
+      if (user.age >= min && user.age <= max) {
+            return user.age;
+      }
+})
+
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+
+
+//TASK 7
+const calculateTotalBalance = users => {
+  // твой код
+};
+
+console.log(calculateTotalBalance(users)); // 20916
